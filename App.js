e15d3833b970fbe084ca90dd1d9b8310809ad705
Sara Correa <saritac.serna@gmail.com>
2025-10-27 16:07:20 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PaperProvider } from 'react-native-paper';
+import LogIn from './src/Screens/Module/LogIn';
+import Register from './src/Screens/Module/Register';
+import Home from './src/Screens/Module/Home';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LogIn">
+          <Stack.Screen name="LogIn" component={LogIn} />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
