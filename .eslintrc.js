b656b257fcf05b28af67ce498c5d3b31a01010d7
Sara Correa <saritac.serna@gmail.com>
2025-10-27 12:56:03 -0500

    added style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'prettier'
+  ],
+  plugins: ['react', 'react-native', 'prettier'],
+  env: {
+    node: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+
+    'react-native/no-inline-styles': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  settings: { react: { version: 'detect' } },
 };
