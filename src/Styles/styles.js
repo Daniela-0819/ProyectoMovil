fcbb835a0f2f7c695a4c4cc90643645261426435
Daniela <danicaslopera2005@gmail.com>
2025-11-02 20:55:45 -0500

    modifications and additions

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -1,10 +1,10 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   //General
   container: {
     flex: 1,
-    backgroundColor: "#faf7ff",
+    backgroundColor: '#faf7ff',
     padding: 16,
   },
 
@@ -13,14 +13,14 @@ const styles = StyleSheet.create({
   },
 
   headerContainer: {
-    alignItems: "center",
+    alignItems: 'center',
     marginVertical: 20,
   },
 
   title: {
     fontSize: 24,
-    fontWeight: "bold",
-    color: "#6C2DC7",
+    fontWeight: 'bold',
+    color: '#6C2DC7',
   },
 
   //FORMULARIOS (Login, Register, PostTweet)
@@ -28,9 +28,9 @@ const styles = StyleSheet.create({
     margin: 15,
     padding: 15,
     borderRadius: 12,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     elevation: 2,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 4,
@@ -38,13 +38,13 @@ const styles = StyleSheet.create({
 
   input: {
     marginBottom: 12,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
 
   button: {
     marginTop: 10,
     borderRadius: 10,
-    backgroundColor: "#9C27B0",
+    backgroundColor: '#9C27B0',
   },
 
   divider: {
@@ -54,15 +54,15 @@ const styles = StyleSheet.create({
   //FEED
   feedTitle: {
     fontSize: 24,
-    fontWeight: "bold",
-    color: "#6C2DC7",
-    textAlign: "center",
+    fontWeight: 'bold',
+    color: '#6C2DC7',
+    textAlign: 'center',
     marginVertical: 20,
   },
 
   emptyText: {
-    textAlign: "center",
-    color: "gray",
+    textAlign: 'center',
+    color: 'gray',
     marginTop: 40,
   },
 
@@ -70,90 +70,112 @@ const styles = StyleSheet.create({
     marginHorizontal: 80,
     marginTop: 20,
     borderRadius: 12,
-    backgroundColor: "#EDE7F6",
+    backgroundColor: '#EDE7F6',
   },
 
   refreshButtonLabel: {
-    color: "#6C2DC7",
-    fontWeight: "600",
+    color: '#6C2DC7',
+    fontWeight: '600',
   },
 
-  //TWEET CARD 
+  //TWEET CARD
   tweetCard: {
     marginBottom: 15,
     borderRadius: 12,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     elevation: 2,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 4,
   },
 
   tweetHeader: {
-    fontWeight: "bold",
-    color: "#6C2DC7",
+    fontWeight: 'bold',
+    color: '#6C2DC7',
     fontSize: 16,
   },
-   tweetHeader02: {
-    fontWeight: "bold",
-    color: "#6C2DC7",
+  tweetHeader02: {
+    fontWeight: 'bold',
+    color: '#6C2DC7',
     fontSize: 12,
   },
 
   tweetContent: {
     marginTop: 14,
     fontSize: 15,
-    color: "#333",
+    color: '#333',
   },
 
   tweetDate: {
     fontSize: 12,
-    color: "#888",
-    textAlign: "right",
+    color: '#888',
+    textAlign: 'right',
     marginTop: 8,
   },
 
-  // PERFIL (ProfileView / Home) 
-  profileContainer: {
-    alignItems: "center",
-    marginVertical: 20,
-  },
-
+  // PERFIL (FeedViwe)
   profileName: {
+    marginTop: 5,
     fontSize: 18,
-    fontWeight: "bold",
-    color: "#6C2DC7",
+    fontWeight: 'bold',
+    color: '#6C2DC7',
   },
 
   profileUsername: {
     fontSize: 14,
-    color: "gray",
+    color: 'gray',
   },
 
-  // FOLLOWERS / FOLLOWING 
+  followContainer: {
+    flexDirection: 'row',
+    marginTop: 6,
+  },
+
+  followButtonProfile: {
+    marginTop: 10,
+    borderRadius: 15,
+    alignSelf: 'flex-start',
+  },
+
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#6C2DC7',
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+
+  followColumn: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    marginRight: 10,
+    gap:2,
+  },
+
+  // FOLLOWERS / FOLLOWING
   followersContainer: {
     flex: 1,
-    backgroundColor: "#faf7ff",
+    backgroundColor: '#faf7ff',
     paddingVertical: 10,
   },
   followersTitle: {
     fontSize: 22,
-    fontWeight: "bold",
-    color: "#6C2DC7",
-    textAlign: "center",
+    fontWeight: 'bold',
+    color: '#6C2DC7',
+    textAlign: 'center',
     marginVertical: 15,
   },
   userCard: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     padding: 12,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderRadius: 15,
     marginVertical: 6,
     marginHorizontal: 16,
     elevation: 2,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOpacity: 0.08,
     shadowOffset: { width: 0, height: 1 },
     shadowRadius: 3,
@@ -163,54 +185,65 @@ const styles = StyleSheet.create({
     marginLeft: 12,
   },
   userFullName: {
-    fontWeight: "bold",
-    color: "#6C2DC7",
+    fontWeight: 'bold',
+    color: '#6C2DC7',
     fontSize: 16,
   },
   userUsername: {
-    color: "gray",
+    color: 'gray',
     fontSize: 14,
   },
   followButton: {
-    borderRadius: 8,
-    backgroundColor: "#EDE7F6",
+    borderRadius: 14,
+    backgroundColor: '#EDE7F6',
   },
   followButtonLabel: {
-    color: "#6C2DC7",
-    fontWeight: "600",
+    color: '#6C2DC7',
+    fontWeight: '600',
   },
 
-  //CONTADORES DE SEGUIDORES 
+  //CONTADORES DE SEGUIDORES
   countersContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
     marginBottom: 15,
   },
   counterBox: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginHorizontal: 10,
   },
   counterNumber: {
-    fontWeight: "bold",
-    color: "#6C2DC7",
+    fontWeight: 'bold',
+    color: '#6C2DC7',
     fontSize: 16,
     marginRight: 4,
   },
   counterLabel: {
-    color: "gray",
+    color: 'gray',
     fontSize: 14,
   },
 
-  userRow: {
-  flexDirection: "row",
-  alignItems: "center",
-  paddingVertical: 10,
-  borderBottomWidth: 0.3,
-  borderColor: "#ccc",
-},
+  //HOME
+  fab: {
+    position: 'absolute',
+    right: 12,
+    bottom: 82,
+    backgroundColor: '#9C27B0',
+  },
 
-
+  bottomNav: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    bottom: 0,
+    width: '110%',
+    backgroundColor: '#faf7ff',
+    paddingVertical: 8,
+    borderTopWidth: 1,
+    borderColor: '#ccc',
+  },
 });
 
 export default styles;
